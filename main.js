aeb2464b79db08957ebadb407acc0ef3356fe6ff
Marvin Caspar <marvin.caspar@gmail.com>
2020-03-18 08:41:22 +0100

    Extract style and script to own file

---
+++ main.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('impressum-modal');
+var btn = document.getElementById("open-impressum-modal");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
